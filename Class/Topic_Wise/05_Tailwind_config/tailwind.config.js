ddeb96ac409d59367d9556390a87d5f3d6f2a91e
Roman Ojha <razzroman98@gmail.com>
2022-03-28 10:58:58 +0545

    Fonts_and_Colors & Tailwind_Config File class

---
+++ Class/Topic_Wise/05_Tailwind_config/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./Topic_Wise/03_Fonts_and_Colors/public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6353",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
