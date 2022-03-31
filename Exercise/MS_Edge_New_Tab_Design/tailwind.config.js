4df2eb228367fb74c6acd119c247bd3f8ecd708d
Roman Ojha <razzroman98@gmail.com>
2022-03-31 23:59:27 +0545

    initilizing ms edge new tab design project using tailwind

---
+++ Exercise/MS_Edge_New_Tab_Design/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "body-background":
+          "url('https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')",
+      },
+    },
+  },
+  plugins: [],
+};
